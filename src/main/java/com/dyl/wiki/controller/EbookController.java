e06c1b27fe308846d5d7b4343410e1d83e9bad23
Dell <805954564@qq.com>
2021-08-05 19:58:29 +0800

    3-7 封装请求参数和和返回参数

--- src/main/java/com/dyl/wiki/controller/EbookController.java
+++ src/main/java/com/dyl/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.dyl.wiki.controller;
 
-import com.dyl.wiki.domain.Ebook;
+import com.dyl.wiki.req.EbookReq;
 import com.dyl.wiki.resp.CommonResp;
+import com.dyl.wiki.resp.EbookResp;
 import com.dyl.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +19,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp List(){
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp List(EbookReq req){
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
--- src/main/java/com/dyl/wiki/service/EbookService.java
+++ src/main/java/com/dyl/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.dyl.wiki.service;
 
 import com.dyl.wiki.domain.Ebook;
+import com.dyl.wiki.domain.EbookExample;
 import com.dyl.wiki.mapper.EbookMapper;
+import com.dyl.wiki.req.EbookReq;
+import com.dyl.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,7 +19,19 @@ public class EbookService {
 //    @Autowired
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list(){
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req){
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + req.getName() + "%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebookList){
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook, ebookResp);
+//            ebookResp.setId(123L);  # 测试修改是否生效
+            respList.add(ebookResp);
+        }
+
+        return respList;
     }
 }
