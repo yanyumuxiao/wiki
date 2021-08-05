81624c42f11c326b8d8b938a57e74e263e3ae3a4
Dell <805954564@qq.com>
2021-08-05 20:16:19 +0800

    3-8 制作CopyUtil封装BeanUtils

--- src/main/java/com/dyl/wiki/service/EbookService.java
+++ src/main/java/com/dyl/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.dyl.wiki.domain.EbookExample;
 import com.dyl.wiki.mapper.EbookMapper;
 import com.dyl.wiki.req.EbookReq;
 import com.dyl.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.dyl.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,14 +23,18 @@ public class EbookService {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList){
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-//            ebookResp.setId(123L);  # 测试修改是否生效
-            respList.add(ebookResp);
-        }
 
-        return respList;
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList){
+////            EbookResp ebookResp = new EbookResp();
+////            BeanUtils.copyProperties(ebook, ebookResp);
+////            ebookResp.setId(123L);  # 测试修改是否生效
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+
+        return list;
     }
 }
